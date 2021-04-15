985443e3bfacf9de1b30fe3fdbd1394bc733e400
Agustín Roca <aroca@itba.edu.ar>
2021-04-15 12:11:42 -0300

    Update Parser.java

--- src/main/java/ar/edu/itba/brownian/parser/Parser.java
+++ src/main/java/ar/edu/itba/brownian/parser/Parser.java
@@ -9,15 +9,13 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Parser {
-    private static final int PROPERTIES_QTY = 7;
-
     public static ParseResults parse(String path) throws FileNotFoundException {
         Scanner scanner = new Scanner(new File(path));
         double spaceSize = Double.parseDouble(scanner.nextLine().trim());
         int particlesQty = Integer.parseInt(scanner.nextLine().trim());
         Set<Particle> particles = new HashSet<>(particlesQty);
         for(int particleNumber = 0; particleNumber < particlesQty; particleNumber++){
-            String[] particleProperties = scanner.nextLine().trim().split(" ", PROPERTIES_QTY);
+            String[] particleProperties = scanner.nextLine().trim().split(" ", ParticleProperties.values().length);
             double mass = Double.parseDouble(particleProperties[ParticleProperties.MASS.order]);
             double radius = Double.parseDouble(particleProperties[ParticleProperties.RADIUS.order]);
             Position position = new Position(Double.parseDouble(particleProperties[ParticleProperties.POSITION_X.order]), Double.parseDouble(particleProperties[ParticleProperties.POSITION_Y.order]));
