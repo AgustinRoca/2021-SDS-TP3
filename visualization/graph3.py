ef10a165f258ac681622e7bf078ef56f8f4fda99
gbarbieripederiva <gbarbieripederiva@gmail.com>
2021-04-20 19:53:43 -0300

    finished graph 3

--- visualization/graph3.py
+++ visualization/graph3.py
@@ -1,25 +1,39 @@
 import matplotlib.pyplot as plt
 from matplotlib.path import Path
 import matplotlib.patches as patches
-import dataparser
+from matplotlib import cm
 
-simdata = dataparser.parse_output_file("../data/output.txt")
-vertices = []
-vertices.append((simdata.particles[0].x,simdata.particles[0].y))
-for e in simdata.events:
-    if e.particles[0].id == 0:
-        vertices.append((e.particles[0].x,e.particles[0].y))
-    elif len(e.particles) == 2 and e.particles[1].id == 0:
-        vertices.append((e.particles[1].x,e.particles[1].y))
+temperature_file = open("../data/temperatures.txt","r")
 
-path = Path(vertices)
+sim_side = float(temperature_file.readline().strip())
 
-fig, ax = plt.subplots()
-patch = patches.PathPatch(path, facecolor="none", lw=2)
-fig.set_size_inches((12,12))
+# even temperatures are for the big one and odd ones are for the small one
+tpaths = []
+line = temperature_file.readline().strip()
+while line and line != '':
+    line = line.split(" ")
+    vertices = []
+    for vertex in line:
+        vertex = vertex.split(":")
+        vertices.append((float(vertex[0]),float(vertex[1])))
+    tpaths.append(Path(vertices,closed=False))
+    line = temperature_file.readline().strip()
 
-ax.add_patch(patch)
-ax.set_xlim(0, simdata.sim_side)
-ax.set_ylim(0, simdata.sim_side)
+ax = plt.gca()
+ax.figure.set_size_inches((12,12))
+
+total_temperatures = len(tpaths)
+color_mapper = cm.get_cmap('nipy_spectral',total_temperatures)
+my_patches = []
+my_labels = []
+for i,p in enumerate(tpaths):
+    pat = patches.PathPatch(p,facecolor="none",edgecolor=color_mapper(i),lw=1)
+    ax.add_patch(pat)
+    my_patches.append(pat)
+    my_labels.append( "Temperatura " + str(i) )
+ax.legend(my_patches,my_labels)
+
+ax.set_xlim(0, sim_side)
+ax.set_ylim(0, sim_side)
 
 plt.show()
